dbd28bb07f989a1f2080b6ba31971e4ae767df0e
RevyaS <revya25@gmail.com>
2025-01-01 13:28:57 +0800

    Added Login Validation

---
+++ web/src/app/api/confirmAdminLogin/route.ts
@@ -0,0 +1,40 @@
+import { CreateConnection  } from "@/config/mariadbConfig";
+
+export async function POST(request: Request)
+{
+    const formData = await request.formData();
+    const guid = formData.get("guid");
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    if(guid == null || username == null || password == null)
+    {
+        return new Response("Bad Request", {
+            status: 400
+        })
+    }
+
+    const result : any = await new Promise((resolve, reject) => {
+        var DB = CreateConnection();
+
+        DB.query("SELECT * FROM Admins WHERE ownerManagementGuid = ? and name = ? and password = PASSWORD(?)", [guid, username, password], 
+        function(err, results) {
+            if(err) {
+                reject(err);
+            }
+            else {
+                resolve(results);
+            }
+        });
+    });
+
+    if(Array.isArray(result) && result.length > 0) {
+        return new Response("Valid Admin", {
+            status: 200
+        });
+    } 
+
+    return new Response("Bad Request", {
+        status: 400
+    })
+}
---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="min-h-screen min-w-screen m-0">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
