f3099d56806f87047c8350039d3314c61f255f4f
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-07 00:42:16 +0800

    fixed any and var eslint warnings in record repo

--- web/src/repositories/mariaDb/RecordRepository.ts
+++ web/src/repositories/mariaDb/RecordRepository.ts
@@ -1,50 +1,58 @@
-import { CreateConnection  } from "@/config/mariadbConfig";
+import { CreateConnection } from "@/config/mariadbConfig";
 
 export class RecordRepository {
-    AddRecord(auditId: number, balance: number, reason: string, receipt: ArrayBuffer, signature: string) {
-        var DB = CreateConnection();
-        const result = DB.execute("INSERT INTO Records (auditId, amount, reason, receipt, signature, approved, dateCreated) VALUES " +
-            "(?, ?, ?, ?, ?, ?, ?)", 
-            [auditId, balance, reason, receipt, signature, 0, new Date()],
-        );
-        return result
-    }
+  AddRecord(
+    auditId: number,
+    balance: number,
+    reason: string,
+    receipt: ArrayBuffer,
+    signature: string
+  ) {
+    const DB = CreateConnection();
+    const result = DB.execute(
+      "INSERT INTO Records (auditId, amount, reason, receipt, signature, approved, dateCreated) VALUES " +
+        "(?, ?, ?, ?, ?, ?, ?)",
+      [auditId, balance, reason, receipt, signature, 0, new Date()]
+    );
+    return result;
+  }
 
-    async GetRecordList(auditId: number) {
-        const result: any = await new Promise((resolve, reject) => {
-            var DB = CreateConnection();
+  async GetRecordList(auditId: number) {
+    const result: unknown = await new Promise((resolve, reject) => {
+      const DB = CreateConnection();
 
-            DB.query("SELECT *, UNIX_TIMESTAMP(dateCreated) as dateCreatedEpoch FROM Records WHERE auditId = ?", [auditId],
-                function (err, results) {
-                    if (err) {
-                        reject(err);
-                    }
-                    else {
-                        resolve(results);
-                    }
-                });
-        });
+      DB.query(
+        "SELECT *, UNIX_TIMESTAMP(dateCreated) as dateCreatedEpoch FROM Records WHERE auditId = ?",
+        [auditId],
+        function (err, results) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
 
-        return result
-    }
+    return result;
+  }
 
-    async UpdateRecord(id: number, receipt: ArrayBuffer, signature: string) {
-        var DB = CreateConnection();
-        const result = DB.execute("UPDATE Records SET " +
-            "receipt = ?, " +
-            "signature = ? " +
-            "WHERE id = ?",
-            [receipt, signature, id],
-        );
+  async UpdateRecord(id: number, receipt: ArrayBuffer, signature: string) {
+    const DB = CreateConnection();
+    const result = DB.execute(
+      "UPDATE Records SET " +
+        "receipt = ?, " +
+        "signature = ? " +
+        "WHERE id = ?",
+      [receipt, signature, id]
+    );
 
-        return result;
-    }
+    return result;
+  }
 
-    async DeleteRecordById(id: number) {
-        var DB = CreateConnection();
-        const result = DB.execute("DELETE FROM Records WHERE id = ?",
-            [id]
-        );
-        return result;
-    }
+  async DeleteRecordById(id: number) {
+    const DB = CreateConnection();
+    const result = DB.execute("DELETE FROM Records WHERE id = ?", [id]);
+    return result;
+  }
 }
