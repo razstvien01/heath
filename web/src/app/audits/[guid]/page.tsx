0e2730fbf7f3910a1a8bc96dd1f7a4f9390eeced
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-09 00:56:29 +0800

    formated code

--- web/src/app/audits/[guid]/page.tsx
+++ web/src/app/audits/[guid]/page.tsx
@@ -1,34 +1,39 @@
 import AuditCrud from "@/components/pages/audits/auditCrud";
 import { notFound } from "next/navigation";
 
-async function isAdminGuid(guid : string) {
-  const isAdminGuidUrl = process.env.NEXT_PUBLIC_API_URL + "/api/owner/isOwnerGuid";
+async function isAdminGuid(guid: string) {
+  const isAdminGuidUrl =
+    process.env.NEXT_PUBLIC_API_URL + "/api/owner/isOwnerGuid";
 
   const formData = new FormData();
   formData.append("guid", guid);
 
   const res = await fetch(isAdminGuidUrl, {
     method: "POST",
-    body: formData
+    body: formData,
   });
 
-  if(res.ok) {
+  if (res.ok) {
     return true;
-  } 
+  }
   return false;
 }
 
-export default async function AuditManagementPage({ params } : { params: { guid : string } }) {
+export default async function AuditManagementPage({
+  params,
+}: {
+  params: { guid: string };
+}) {
   const { guid } = await params;
 
   const isAdminGuidRes = await isAdminGuid(guid);
-    if(!isAdminGuidRes) {
-        notFound();
-    }
+  if (!isAdminGuidRes) {
+    notFound();
+  }
 
   return (
     <div>
-       <AuditCrud guid={guid} />
+      <AuditCrud guid={guid} />
     </div>
-  )
-};
+  );
+}
