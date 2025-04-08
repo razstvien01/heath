bab3d41bb8ff04200926456ddc741ea76f905f6a
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-09 00:57:01 +0800

    formatted code file

--- web/src/app/owners/[guid]/page.tsx
+++ web/src/app/owners/[guid]/page.tsx
@@ -1,34 +1,39 @@
 import OwnerCrud from "@/components/pages/owners/ownerCrud";
 import { notFound } from "next/navigation";
 
-async function isAdminGuid(guid : string) {
-  const isAdminGuidUrl = process.env.NEXT_PUBLIC_API_URL + "/api/admin/isAdminGuid";
+async function isAdminGuid(guid: string) {
+  const isAdminGuidUrl =
+    process.env.NEXT_PUBLIC_API_URL + "/api/admin/isAdminGuid";
 
   const formData = new FormData();
   formData.append("guid", guid);
 
   const res = await fetch(isAdminGuidUrl, {
     method: "POST",
-    body: formData
+    body: formData,
   });
 
-  if(res.ok) {
+  if (res.ok) {
     return true;
-  } 
+  }
   return false;
 }
 
-export default async function OwnerManagementPage({ params } : { params: { guid : string } }) {
+export default async function OwnerManagementPage({
+  params,
+}: {
+  params: { guid: string };
+}) {
   const { guid } = await params;
 
   const isAdminGuidRes = await isAdminGuid(guid);
-  if(!isAdminGuidRes) {
+  if (!isAdminGuidRes) {
     notFound();
   }
 
   return (
     <div>
-        <OwnerCrud guid={guid} />
+      <OwnerCrud guid={guid} />
     </div>
-  )
-};
+  );
+}
--- web/src/app/records/[guid]/page.tsx
+++ web/src/app/records/[guid]/page.tsx
@@ -7,57 +7,59 @@ interface PageInfo {
   isInvalid: boolean;
 }
 
-async function isPublicGuid(guid : string) {
-  const isPublicGuidUrl = process.env.NEXT_PUBLIC_API_URL + "/api/audit/isPublicGuid";
+async function isPublicGuid(guid: string) {
+  const isPublicGuidUrl =
+    process.env.NEXT_PUBLIC_API_URL + "/api/audit/isPublicGuid";
 
   const formData = new FormData();
   formData.append("guid", guid);
 
   let isInvalid = false;
 
-  const val : boolean = await new Promise((resolve, reject) => {
+  const val: boolean = await new Promise((resolve, reject) => {
     fetch(isPublicGuidUrl, {
       method: "POST",
-      body: formData
+      body: formData,
     })
-    .then(res => {
-      isInvalid = !res.ok
-      return res.json()
-    })
-    .then(data => {
-      resolve(data)
-    }).catch(err => {
-      reject(err)
-    })
+      .then((res) => {
+        isInvalid = !res.ok;
+        return res.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 
-  var output : PageInfo = {
+  const output: PageInfo = {
     isInvalid: isInvalid,
     isPublic: val,
-  }
+  };
 
   return output;
 }
 
-export default async function AuditManagementPage({ params } : { params: { guid : string } }) {
+export default async function AuditManagementPage({
+  params,
+}: {
+  params: { guid: string };
+}) {
   const { guid } = await params;
 
   const isPublicGuidRes = await isPublicGuid(guid);
-  
-  console.log(isPublicGuidRes)
 
-  if(isPublicGuidRes.isInvalid) {
-      notFound();
+  console.log(isPublicGuidRes);
+
+  if (isPublicGuidRes.isInvalid) {
+    notFound();
   }
 
   return (
     <div>
-      {!isPublicGuidRes.isPublic && (
-        <PrivateRecordCrud guid={guid} />
-      )}
-      {isPublicGuidRes.isPublic && (
-        <RecordCrud guid={guid} />
-      )}
+      {!isPublicGuidRes.isPublic && <PrivateRecordCrud guid={guid} />}
+      {isPublicGuidRes.isPublic && <RecordCrud guid={guid} />}
     </div>
-  )
-};
+  );
+}
