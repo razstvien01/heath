24d8ab9ce67fd2781e72f6e91bd89ce3ddbe76aa
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-11 23:50:58 +0800

    refactored and adjust code in owner repo

--- web/src/repositories/mariaDb/OwnerRepository.ts
+++ web/src/repositories/mariaDb/OwnerRepository.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 export class OwnerRepository {
   async AddOwner(username: string, password: string) {
     const DB = CreateConnection();
-    const result = DB.execute(
+    const result = (await DB).execute(
       "INSERT INTO Owners (name, password, managementGuid) VALUES " +
         "(?, SHA2(?, 256), ?)",
       [username, password, uuidv4()]
@@ -14,56 +14,37 @@ export class OwnerRepository {
   }
 
   async ConfirmOwnerLogin(guid: string, username: string, password: string) {
-    const result: unknown = await new Promise((resolve, reject) => {
-      const DB = CreateConnection();
-
-      DB.query(
-        "SELECT * FROM Owners WHERE managementGuid = ? and name = ? and password = SHA2(?, 256)",
-        [guid, username, password],
-        function (err, results) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(results);
-          }
-        }
-      );
-    });
+    const DB = await CreateConnection();
+    const result: unknown[] = await DB.query(
+      "SELECT * FROM Owners WHERE managementGuid = ? and name = ? and password = SHA2(?, 256)",
+      [guid, username, password]
+    );
 
     return Array.isArray(result) && result.length > 0;
   }
 
   async DeleteOwnerFromManagementGuid(guid: string) {
     const DB = CreateConnection();
-    const result = DB.execute("DELETE FROM Owners WHERE managementGuid = ?", [
+    const result = (await DB).execute("DELETE FROM Owners WHERE managementGuid = ?", [
       guid,
     ]);
     return result;
   }
 
   async IsOwnerGuid(guid: string) {
-    const DB = CreateConnection();
-    const result: unknown = await new Promise((resolve, reject) => {
-      DB.query(
-        "SELECT * FROM Owners WHERE managementGuid = ?",
-        [guid],
-        function (err, results) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(results);
-          }
-        }
-      );
-    });
+    const DB = await CreateConnection();
+    const result: unknown[] = await DB.query(
+      "SELECT * FROM Owners WHERE managementGuid = ?",
+      [guid]
+    );
 
     return Array.isArray(result) && result.length > 0;
   }
 
   async GetOwnerList() {
     const DB = CreateConnection();
-    const result: unknown = await new Promise((resolve, reject) => {
-      DB.query("SELECT * FROM Owners", function (err, results) {
+    const result: unknown = await new Promise(async (resolve, reject) => {
+      (await DB).query("SELECT * FROM Owners", function (err: Error | null, results: unknown) {
         if (err) {
           reject(err);
         } else {
@@ -75,20 +56,8 @@ export class OwnerRepository {
   }
 
   async GetOwnerIdFromManagementGuid(guid: string): Promise<number | null> {
-    const DB = CreateConnection();
-    const rows: unknown = await new Promise((resolve, reject) => {
-      DB.query(
-        "SELECT id FROM Owners WHERE managementGuid = ?",
-        [guid],
-        function (err, results) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(results);
-          }
-        }
-      );
-    });
+    const DB = await CreateConnection();
+    const [rows]: unknown[] = await DB.query("SELECT id FROM Owners WHERE managementGuid = ?", [guid]);
 
     if (Array.isArray(rows) && rows.length > 0) {
       return rows[0].id;
@@ -99,7 +68,7 @@ export class OwnerRepository {
 
   async UpdateOwner(username: string, password: string, guid: string) {
     const DB = CreateConnection();
-    const result = DB.execute(
+    const result = (await DB).execute(
       "UPDATE Owners SET " +
         "name = ?, " +
         "password = SHA2(?, 256) " +
