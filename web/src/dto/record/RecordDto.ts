a3d75052e2aa1b22685f756110a3e802d590756e
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-28 01:35:17 +0800

    implemented record dto

---
+++ web/src/dto/record/RecordDto.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const RecordSchema = z.object({
+  id: z.coerce.number().optional(),
+  auditId: z.number().optional(),
+  amount: z.number().optional(),
+  reason: z.string().max(500).optional(),
+  receipt: z.instanceof(Buffer).optional(),
+  signature: z.string().max(100).optional(),
+  approved: z.number().optional(),
+  createdAt: z.date().default(new Date()).optional(),
+  updatedAt: z.date().default(new Date()).optional(),
+});
+
+export type RecordDto = z.infer<typeof RecordSchema>;
