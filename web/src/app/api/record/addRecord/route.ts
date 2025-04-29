6e07f717fe752c816ef1399523e7980446e54c04
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-30 02:24:40 +0800

    modified record add route and repo and working

--- web/src/app/api/record/addRecord/route.ts
+++ web/src/app/api/record/addRecord/route.ts
@@ -1,8 +1,10 @@
+import { CreateConnection } from "@/config/mariadbConfig";
+import { CreateRecordSchema } from "@/dto/record/CreateRecordReqDto";
 import { AuditRepository } from "@/repositories/mariaDb/AuditRepository";
 import { RecordRepository } from "@/repositories/mariaDb/RecordRepository";
 
-export async function POST(request: Request)
-{
+export async function POST(request: Request) {
+  try {
     const formData = await request.formData();
 
     const guid = formData.get("guid");
@@ -11,42 +13,62 @@ export async function POST(request: Request)
     const receiptFile = formData.get("receipt");
     const signature = formData.get("signature");
 
-    let receipt = null
-    if (receiptFile) {
-        receipt = await new Promise((resolve, reject) => {
-            (receiptFile as File).arrayBuffer().then((arrayBuffer) => {
-                resolve(arrayBuffer)
-            })
-        })
+    if (!guid || !balance || !reason || !signature) {
+      return new Response("Missing required fields", {
+        status: 400,
+      });
     }
 
-    if(guid == null || balance == null || reason == null)
-    {
-        return new Response("Bad", {
-            status: 400
-        })
+    const receipt =
+      receiptFile instanceof File
+        ? Buffer.from(await receiptFile.arrayBuffer())
+        : undefined;
+
+    const db = await CreateConnection();
+    const auditRepository = new AuditRepository(db);
+    const auditId = await auditRepository.getAuditIdFromGuid(guid as string);
+
+    if (!auditId) {
+      return new Response("Invalid request. Audit not found", {
+        status: 400,
+      });
     }
 
-    const auditRepository = new AuditRepository();
-    const recordRepository = new RecordRepository();
+    const parsedRecord = CreateRecordSchema.safeParse({
+      auditId,
+      amount: Number(balance),
+      reason,
+      receipt,
+      signature,
+      approved: 0,
+    });
 
-    const auditId = await auditRepository.GetAuditIdFromGuid(guid as string)
-
-    if(!auditId) {
-        return new Response("Invalid Request", {
-            status: 500
-        })
-    } 
-
-    const result = recordRepository.AddRecord(auditId, Number(balance), reason as string, receipt as ArrayBuffer, signature as string)
-
-    if(result) {
-        return new Response("Record inserted successfully", {
-            status: 200
-        });
-    } else {
-        return new Response("Record insert failed", {
-            status: 500
-        })
+    if (!parsedRecord.success) {
+      return new Response(
+        `Bad Request: ${parsedRecord.error.issues[0].message}`,
+        {
+          status: 400,
+        }
+      );
     }
+
+    const recordRepository = new RecordRepository(db);
+    const result = await recordRepository.addRecord(parsedRecord.data);
+
+    if (!result) {
+      throw Error("Failed to insert record");
+    }
+
+    return new Response("Record inserted successfully", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    const message =
+      error instanceof Error ? error.message : "Internal Server Error";
+
+    return new Response(message, {
+      status: 500,
+    });
+  }
 }
--- web/src/repositories/mariaDb/RecordRepository.ts
+++ web/src/repositories/mariaDb/RecordRepository.ts
@@ -1,48 +1,87 @@
-import { CreateConnection } from "@/config/mariadbConfig";
+import {
+  CreateRecordReqDto,
+  RecordDto,
+  UpdateRecordReqDto,
+} from "@/dto/record";
+import { IRecordRepository } from "@/interfaces";
+import { Connection, QueryResult, FieldPacket } from "mysql2/promise";
 
-export class RecordRepository {
-  async AddRecord(
-    auditId: number,
-    balance: number,
-    reason: string,
-    receipt: ArrayBuffer,
-    signature: string
-  ) {
-    const DB = CreateConnection();
-    const result = (await DB).execute(
-      "INSERT INTO Records (auditId, amount, reason, receipt, signature, approved) VALUES " +
-        "(?, ?, ?, ?, ?, ?)",
-      [auditId, balance, reason, receipt, signature, 0]
-    );
-    return result;
+export class RecordRepository implements IRecordRepository {
+  private readonly _db: Connection;
+
+  constructor(db: Connection) {
+    this._db = db;
   }
 
-  async GetRecordList(auditId: number) {
-    const DB = CreateConnection();
-    const [results]: unknown[] = await (await DB).query(
-      "SELECT *, UNIX_TIMESTAMP(dateCreated) as dateCreatedEpoch FROM Records WHERE auditId = ?",
-      [auditId]
-    );
+  async addRecord(
+    dto: CreateRecordReqDto
+  ): Promise<[QueryResult, FieldPacket[]]> {
+    const query =
+      "INSERT INTO Records (auditId, amount, reason, receipt, signature, approved) VALUES (?, ?, ?, ?, ?, ?)";
+    const values = [
+      dto.auditId,
+      dto.amount,
+      dto.reason,
+      dto.receipt,
+      dto.signature,
+      dto.approved,
+    ];
 
-    return results;
-  }
-
-  async UpdateRecord(id: number, receipt: ArrayBuffer, signature: string) {
-    const DB = CreateConnection();
-    const result = (await DB).execute(
-      "UPDATE Records SET " +
-        "receipt = ?, " +
-        "signature = ? " +
-        "WHERE id = ?",
-      [receipt, signature, id]
-    );
+    const result = await this._db.execute(query, values);
 
     return result;
   }
-
-  async DeleteRecordById(id: number) {
-    const DB = CreateConnection();
-    const result = (await DB).execute("DELETE FROM Records WHERE id = ?", [id]);
-    return result;
+  getRecordList(auditId: number): Promise<RecordDto[]> {
+    throw new Error("Method not implemented.");
   }
+  updateRecord(dto: UpdateRecordReqDto): Promise<[QueryResult, FieldPacket[]]> {
+    throw new Error("Method not implemented.");
+  }
+  deleteRecordById(id: number): Promise<[QueryResult, FieldPacket[]]> {
+    throw new Error("Method not implemented.");
+  }
+  // async AddRecord(
+  //   auditId: number,
+  //   balance: number,
+  //   reason: string,
+  //   receipt: ArrayBuffer,
+  //   signature: string
+  // ) {
+  //   const DB = CreateConnection();
+  //   const result = (await DB).execute(
+  //     "INSERT INTO Records (auditId, amount, reason, receipt, signature, approved) VALUES " +
+  //       "(?, ?, ?, ?, ?, ?)",
+  //     [auditId, balance, reason, receipt, signature, 0]
+  //   );
+  //   return result;
+  // }
+
+  // async GetRecordList(auditId: number) {
+  //   const DB = CreateConnection();
+  //   const [results]: unknown[] = await (await DB).query(
+  //     "SELECT *, UNIX_TIMESTAMP(dateCreated) as dateCreatedEpoch FROM Records WHERE auditId = ?",
+  //     [auditId]
+  //   );
+
+  //   return results;
+  // }
+
+  // async UpdateRecord(id: number, receipt: ArrayBuffer, signature: string) {
+  //   const DB = CreateConnection();
+  //   const result = (await DB).execute(
+  //     "UPDATE Records SET " +
+  //       "receipt = ?, " +
+  //       "signature = ? " +
+  //       "WHERE id = ?",
+  //     [receipt, signature, id]
+  //   );
+
+  //   return result;
+  // }
+
+  // async DeleteRecordById(id: number) {
+  //   const DB = CreateConnection();
+  //   const result = (await DB).execute("DELETE FROM Records WHERE id = ?", [id]);
+  //   return result;
+  // }
 }
