06bdb850ac236dae38727935273ffab40ea2749d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-03 15:31:12 +0800

    added isRecordExists abstract func

--- web/src/interfaces/IRecordRepository.ts
+++ web/src/interfaces/IRecordRepository.ts
@@ -8,7 +8,11 @@ import { FieldPacket, QueryResult } from "mysql2/promise";
 
 export interface IRecordRepository {
   addRecord(dto: CreateRecordReqDto): Promise<[QueryResult, FieldPacket[]]>;
-  getRecordList(auditId: number, filters: RecordFilterDto): Promise<RecordDto[]>;
+  getRecordList(
+    auditId: number,
+    filters: RecordFilterDto
+  ): Promise<RecordDto[]>;
   updateRecord(dto: UpdateRecordReqDto): Promise<[QueryResult, FieldPacket[]]>;
   deleteRecordById(id: number): Promise<[QueryResult, FieldPacket[]]>;
+  isRecordExists(id: number): Promise<boolean>;
 }
