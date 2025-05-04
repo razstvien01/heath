6ff2ece032436578ca770a1bf817fe2d4a0645d3
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-04 21:02:44 +0800

    delete record route successfully refactored

--- web/src/app/api/record/deleteRecord/route.ts
+++ web/src/app/api/record/deleteRecord/route.ts
@@ -1,28 +1,44 @@
+import { CreateConnection } from "@/config/mariadbConfig";
+import { RecordSchema } from "@/dto/record/RecordDto";
 import { RecordRepository } from "@/repositories/mariaDb/RecordRepository";
 
-export async function POST(request: Request)
-{
+export async function DELETE(request: Request): Promise<Response> {
+  try {
     const formData = await request.formData();
     const id = formData.get("id");
 
-    if(id == null)
-    {
-        return new Response("Bad", {
-            status: 400
-        })
+    if (!id) {
+      return new Response("Bad", {
+        status: 400,
+      });
     }
 
-    const repository = new RecordRepository();
+    const validation = RecordSchema.safeParse({
+      id,
+    });
 
-    const result = await repository.DeleteRecordById(Number(id));
+    if (!validation.success)
+      return new Response(
+        `Bad Request: ${validation.error.issues[0].message}`,
+        {
+          status: 400,
+        }
+      );
 
-    if(result) {
-        return new Response("Record deleted successfully", {
-            status: 200
-        });
-    } else {
-        return new Response("Record delete failed", {
-            status: 500
-        })
-    }
+    const db = await CreateConnection();
+    const recordRepository = new RecordRepository(db);
+    await recordRepository.deleteRecordById(Number(id));
+
+    return new Response("Record deleted successfully", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    const message =
+      error instanceof Error ? error.message : "Internal Server Error";
+
+    return new Response(message, {
+      status: message.includes("not found") ? 400 : 500,
+    });
+  }
 }
