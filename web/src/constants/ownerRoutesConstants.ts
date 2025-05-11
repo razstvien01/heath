f520425362ff400c1a99f623579fd037dccf40d4
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-11 17:29:26 +0800

    rename urls to routes

--- web/src/constants/ownerRoutesConstants.ts
+++ web/src/constants/ownerRoutesConstants.ts
@@ -1,8 +1,8 @@
-
-
 export const OwnerRoutes = {
-  GET_OWNERS_URL: process.env.NEXT_PUBLIC_API_URL + "/api/owner/ownersList",
-  ADD_OWNER_URL: process.env.NEXT_PUBLIC_API_URL + "/api/owner/addOwner",
-  UPDATE_OWNER_URL: process.env.NEXT_PUBLIC_API_URL + "/api/owner/updateOwner",
-  DELETE_OWNER_URL: process.env.NEXT_PUBLIC_API_URL + "/api/owner/deleteOwner"
+  GET_OWNERS_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/ownersList",
+  ADD_OWNER_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/addOwner",
+  UPDATE_OWNER_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/updateOwner",
+  DELETE_OWNER_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/deleteOwner",
+  TOTAL_COUNTS_ROUTE:
+    process.env.NEXT_PUBLIC_API_URL + "/api/owner/totalOwners",
 };
