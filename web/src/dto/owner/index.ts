2aa855cc149bd51d6e214561634cfedcd9ebf1c1
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-17 22:22:30 +0800

    removed OwnerCountFilter dto

--- web/src/dto/owner/index.ts
+++ web/src/dto/owner/index.ts
@@ -3,4 +3,3 @@ export type { OwnerDto } from "./OwnerDto";
 export type { UpdateOwnerReqDto } from "./UpdateOwnerReqDto";
 export type { OwnerFilterDto } from "./OwnerFIlterDto";
 export type { ConfirmOwnerReqDto } from "./ConfirmOwnerReqDto";
-export type { OwnerCountFilterDto } from "./OwnerCountFilterDto";
