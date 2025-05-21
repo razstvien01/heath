eb48c542844ba243f680a818a41490ef0c1b0ce0
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-21 14:18:16 +0800

    imported the themeMOdeToggle and tested

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Wallet } from "lucide-react";
+import { ThemeModeToggle } from "@/components/themeModeToggle";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import ThemeDataProvider from "@/contexts/theme-data-provider";
+import { ThemeColorToggle } from "@/components/themeColorToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,19 +28,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased before:dark:to-primary`}
       >
-        <header className="bg-white border-b py-4 px-6 shadow-sm">
-          <div className="container mx-auto flex justify-between items-center">
-            <div className="flex items-center gap-2">
-              <Wallet className="h-8 w-8" />
-              <span className="text-2xl font-bold">Heath</span>
-            </div>
-          </div>
-        </header>
-        {children}
+        {/* <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        > */}
+        {/* </ThemeProvider> */}
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ThemeDataProvider>
+            <header className="border-b py-4 px-6 shadow-sm">
+              <div className="container mx-auto flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  <Wallet className="h-8 w-8" />
+                  <span className="text-2xl font-bold">Heath</span>
+                  <ThemeColorToggle/>
+                  <ThemeModeToggle />
+                </div>
+              </div>
+            </header>
+            {children}
+          </ThemeDataProvider>
+        </NextThemesProvider>
       </body>
     </html>
   );
