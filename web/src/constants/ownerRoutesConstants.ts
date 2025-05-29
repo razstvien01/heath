60688d3b93405ffca0644641371b7bcf202a1fac
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-30 04:49:00 +0800

    added IS_OWNER_GUID & CONFIRM_OWNER_URL constants

--- web/src/constants/ownerRoutesConstants.ts
+++ web/src/constants/ownerRoutesConstants.ts
@@ -1,8 +1,13 @@
 export const OwnerRoutes = {
   GET_OWNERS_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/ownersList",
   ADD_OWNER_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/addOwner",
-  UPDATE_OWNER_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/updateOwner",
-  DELETE_OWNER_ROUTE: process.env.NEXT_PUBLIC_API_URL + "/api/owner/deleteOwner",
+  UPDATE_OWNER_ROUTE:
+    process.env.NEXT_PUBLIC_API_URL + "/api/owner/updateOwner",
+  DELETE_OWNER_ROUTE:
+    process.env.NEXT_PUBLIC_API_URL + "/api/owner/deleteOwner",
   TOTAL_COUNTS_ROUTE:
     process.env.NEXT_PUBLIC_API_URL + "/api/owner/totalOwners",
+  IS_OWNER_GUID: process.env.NEXT_PUBLIC_API_URL + "/api/owner/isOwnerGuid",
+  CONFIRM_OWNER_URL:
+    process.env.NEXT_PUBLIC_API_URL + "/api/owner/confirmOwnerLogin",
 };
