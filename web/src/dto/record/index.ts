01910966f839af995c177e4561ef91ae5dfb595f
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-01 21:51:12 +0800

    added CreateBackdatedReqDto

--- web/src/dto/record/index.ts
+++ web/src/dto/record/index.ts
@@ -2,3 +2,4 @@ export type { RecordDto } from "./RecordDto";
 export type { CreateRecordReqDto } from "./CreateRecordReqDto";
 export type { UpdateRecordReqDto } from "./UpdateRecordReqDto";
 export type { RecordFilterDto } from "./RecordFilterDto";
+export type { CreateBackdatedReqDto } from "./CreateBackdatedReqDto";
