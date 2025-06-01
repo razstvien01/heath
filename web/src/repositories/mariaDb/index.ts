a0d3f3b779d0644251861163e706ecb656a20957
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-01 14:55:21 +0800

    added index.ts exporter in the repository

---
+++ web/src/repositories/mariaDb/index.ts
@@ -0,0 +1,4 @@
+export { OwnerRepository } from "@/repositories/mariaDb/OwnerRepository";
+export { AuditRepository } from "@/repositories/mariaDb/AuditRepository";
+export { RecordRepository } from "@/repositories/mariaDb/RecordRepository";
+export { AdminRepository } from "@/repositories/mariaDb/AdminRepository";
