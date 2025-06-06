9d38f6a61555e70a5ab6c0b807fcaf785bea10dc
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-06 13:55:01 +0800

    rename to toRecordDto, and added toRecordUrlDto

--- web/src/mappers/RecordMapper.ts
+++ web/src/mappers/RecordMapper.ts
@@ -2,7 +2,7 @@ import { RecordDto } from "@/dto/record";
 import Record from "@/models/Record";
 
 export default class RecordMapper {
-  public static toOwnerDto(data: Record): RecordDto {
+  public static toRecordDto(data: Record): RecordDto {
     return {
       amount: data.amount ?? 0,
       reason: data.reason ?? "",
@@ -13,6 +13,20 @@ export default class RecordMapper {
       updatedAt: data.updatedAt ?? new Date(),
       auditId: data.auditId ?? 0,
       id: data.id ?? 0,
+    };
+  }
+
+  public static toRecordUrlDto(data: Record) {
+    return {
+      amount: data.amount ?? 0,
+      reason: data.reason ?? "",
+      signature: data.signature ?? "",
+      approved: data.approved ?? 0,
+      createdAt: data.createdAt ?? new Date(),
+      updatedAt: data.updatedAt ?? new Date(),
+      auditId: data.auditId ?? 0,
+      id: data.id ?? 0,
+      receiptUrl: "api/image/" + data.id,
     };
   }
 }
