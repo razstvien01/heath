84ead4054e0119008cb94bf7fc0224f06163bb1e
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-06 13:47:48 +0800

    added getReceiptById

--- web/src/interfaces/IRecordRepository.ts
+++ web/src/interfaces/IRecordRepository.ts
@@ -9,16 +9,16 @@ import { FieldPacket, QueryResult } from "mysql2/promise";
 
 export interface IRecordRepository {
   addRecord(dto: CreateRecordReqDto): Promise<[QueryResult, FieldPacket[]]>;
-  addBackdated(dto: CreateBackdatedReqDto): Promise<[QueryResult, FieldPacket[]]>;
+  addBackdated(
+    dto: CreateBackdatedReqDto
+  ): Promise<[QueryResult, FieldPacket[]]>;
   getRecordList(
     auditId: number,
     filters: RecordFilterDto
   ): Promise<RecordDto[]>;
-  getRecords(
-    auditId: number,
-    filters: RecordFilterDto
-  ): Promise<RecordDto[]>;
+  getRecords(auditId: number, filters: RecordFilterDto): Promise<RecordDto[]>;
   updateRecord(dto: UpdateRecordReqDto): Promise<[QueryResult, FieldPacket[]]>;
   deleteRecordById(id: number): Promise<[QueryResult, FieldPacket[]]>;
   isRecordExists(id: number): Promise<boolean>;
+  getReceiptById(id: number): Promise<Buffer | null>;
 }
