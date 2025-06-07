8c00946f5df8fe160aa25bde81a1c3326c0bf484
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-07 16:17:22 +0800

    added index for exporting contants

---
+++ web/src/constants/index.ts
@@ -0,0 +1,4 @@
+export { Config } from './configConstants';
+export { AdminRoutes } from "./adminRoutesConstants";
+export { AuditRoutes } from "./auditRoutesConstants";
+export { OwnerRoutes } from "./ownerRoutesConstants";
