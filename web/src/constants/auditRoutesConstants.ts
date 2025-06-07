589d6562d8ed76a4eb1a06135ded6bfed2e425a1
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-07 16:17:07 +0800

    created api routes for the audit

---
+++ web/src/constants/auditRoutesConstants.ts
@@ -0,0 +1,11 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if(!BASE_URL){
+  throw new Error("Missing NEXT_PUBLIC_API_URL in environment variables");
+}
+
+const AUDIT_URL = BASE_URL + "/api/audit";
+
+export const AuditRoutes = Object.freeze({
+  AUDIT_LIST: `${AUDIT_URL}/auditList`,
+})
