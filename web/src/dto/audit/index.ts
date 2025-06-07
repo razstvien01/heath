211e2c1a86f310d81f0b2f37bb28490520a70748
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-07 16:19:06 +0800

    adding AuditFilterDto

--- web/src/dto/audit/index.ts
+++ web/src/dto/audit/index.ts
@@ -1,3 +1,4 @@
 export type { AuditDto } from "./AuditDto";
 export type { CreateAuditReqDto } from "./CreateAuditReqDto";
 export type { UpdateAuditReqDto } from "./UpdateAuditReqDto.";
+export type { AuditFilterDto } from "./AuditFilterDto";
