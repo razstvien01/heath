09afe37e02b496f70793b8a622bcbae8a57ed0eb
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-07 16:19:46 +0800

    added getAuditTotalCount function

--- web/src/interfaces/IAuditRepository.ts
+++ web/src/interfaces/IAuditRepository.ts
@@ -6,11 +6,10 @@ import { QueryResult, FieldPacket } from "mysql2";
 export interface IAuditRepository {
   addAudit(dto: CreateAuditReqDto): Promise<[QueryResult, FieldPacket[]]>;
   getAuditList(ownerId: number, filters: AuditFilterDto): Promise<AuditDto[]>;
-  deleteAuditById(
-    auditId: number
-  ): Promise<[QueryResult, FieldPacket[]]>;
+  deleteAuditById(auditId: number): Promise<[QueryResult, FieldPacket[]]>;
   isAuditPublicGuid(guid: string): Promise<boolean>;
   isAuditPrivateGuid(guid: string): Promise<boolean>;
   updateAudit(dto: UpdateAuditReqDto): Promise<[QueryResult, FieldPacket[]]>;
   getAuditIdFromGuid(guid: string): Promise<number | null>;
+  getAuditTotalCount(ownerId: number, filters: AuditFilterDto): Promise<number>;
 }
