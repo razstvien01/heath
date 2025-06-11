2ae175800e2f0e268893a8f1d18e2ff2a7230e66
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-11 22:27:24 +0800

    added description field in the model Audit

--- web/src/models/Audit.ts
+++ web/src/models/Audit.ts
@@ -1,6 +1,7 @@
 export class Audit {
   id?: number | undefined;
   name?: string = "";
+  description?: string = "";
   publicGuid?: string = "";
   ownerGuid?: string = "";
   createdAt?: Date = new Date();
