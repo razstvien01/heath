ed63d53c2e738de72f5e0b82d1893e79314485a8
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-12 09:38:49 +0800

    removed isPublicGuid func

--- web/src/app/records/[guid]/page.tsx
+++ web/src/app/records/[guid]/page.tsx
@@ -1,45 +1,7 @@
 import PrivateRecordCrud from "@/components/pages/privateRecordCrud";
 import RecordCrud from "@/components/pages/records/recordCrud";
+import { isPublicGuid } from "@/services/recordService";
 import { notFound } from "next/navigation";
-
-interface PageInfo {
-  isPublic: boolean;
-  isInvalid: boolean;
-}
-
-async function isPublicGuid(guid: string) {
-  const isPublicGuidUrl =
-    process.env.NEXT_PUBLIC_API_URL + "/api/audit/isPublicGuid";
-
-  const formData = new FormData();
-  formData.append("guid", guid);
-
-  let isInvalid = false;
-
-  const val: boolean = await new Promise((resolve, reject) => {
-    fetch(isPublicGuidUrl, {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => {
-        isInvalid = !res.ok;
-        return res.json();
-      })
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-
-  const output: PageInfo = {
-    isInvalid: isInvalid,
-    isPublic: val,
-  };
-
-  return output;
-}
 
 export default async function AuditManagementPage({
   params,
@@ -49,8 +11,6 @@ export default async function AuditManagementPage({
   const { guid } = await params;
 
   const isPublicGuidRes = await isPublicGuid(guid);
-
-  console.log(isPublicGuidRes);
 
   if (isPublicGuidRes.isInvalid) {
     notFound();
