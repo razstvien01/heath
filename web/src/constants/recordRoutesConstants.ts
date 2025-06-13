c6423ec935113ecd1f2cb974411206f2a5811f2d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-13 22:45:05 +0800

    added recordList const route

--- web/src/constants/recordRoutesConstants.ts
+++ web/src/constants/recordRoutesConstants.ts
@@ -8,6 +8,7 @@ const RECORD_URL = BASE_URL + "/api/record";
 
 export const RecordRoutes = Object.freeze({
   FETCH_RECORDS: `${RECORD_URL}/fetchRecords`,
+  RECORD_LIST: `${RECORD_URL}/recordList`,
   ADD_RECORD: `${RECORD_URL}/addRecord`,
-  EDIT_RECORD: `${RECORD_URL}/editRecord`
+  EDIT_RECORD: `${RECORD_URL}/editRecord`,
 });
